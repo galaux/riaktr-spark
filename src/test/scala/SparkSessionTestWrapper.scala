37101393279c508c72f84c54362dedadc8951812
Guillaume ALAUX <guillaume@alaux.net>
2018-09-14 12:41:56 +0200

    Remove unused file

--- src/test/scala/SparkSessionTestWrapper.scala
+++
@@ -1,9 +0,0 @@
-import org.apache.spark.sql.SparkSession
-
-trait SparkSessionTestWrapper {
-
-//  lazy val spark: SparkSession = {
-//    SparkSession.builder().master("local").appName("spark session").getOrCreate()
-//  }
-
-}
